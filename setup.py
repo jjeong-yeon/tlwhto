73b0a3f8b307d76f898c0d6409bdc5cc39d0bbdf
jjeong-yeon <100738535+jjeong-yeon@users.noreply.github.com>
2022-05-04 09:26:33 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup (
+  unclude_package_data = True,
+  name ='tlwhto0001',
+  version='0.0.1',
+  description='oss-dev my tlwhto 0001',
+  author='jjeong-yeon
+ ',
+  author_email='kuy09082@naver.com',
+  url = "https://github.com/jjeong-yeon/tlwhto",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
